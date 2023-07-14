6a348988c38b2b39625252f1c9db5a0d434137e6
mahi7000 <mahlet7000@gmail.com>
2023-07-14 05:13:22 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+        char alpha[26] = "abcdfghijklmnoprstuvwxyz";
+        int i;
+
+        for (i = 0; i < 26; i++)
+        {
+                putchar(alpha[i]);
+        }
+        putchar('\n');
+        return (0);
+}
