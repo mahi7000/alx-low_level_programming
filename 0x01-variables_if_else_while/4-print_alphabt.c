c04557f8aa8441059ea8cc19759eeef739b03217
mahi7000 <mahlet7000@gmail.com>
2023-07-14 05:19:12 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,13 @@
 
 int main(void)
 {
-        char alpha[24] = "abcdfghijklmnoprstuvwxyz";
-        int i;
+	char alpha[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
 
-        for (i = 0; i < 24; i++)
-        {
-                putchar(alpha[i]);
-        }
-        putchar('\n');
-        return (0);
+	for (i = 0; i < 24; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
 }
