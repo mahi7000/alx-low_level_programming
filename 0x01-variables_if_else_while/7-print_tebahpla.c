d73f8e02dcd18ff4f6652d899a61ee93d89358f2
mahi7000 <mahlet7000@gmail.com>
2023-07-14 05:57:14 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 126; i > 99; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
