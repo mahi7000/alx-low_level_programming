d88d2a6ae2f2adcc9e00e58dc524cd083937ebf4
mahi7000 <mahlet7000@gmail.com>
2023-07-18 04:13:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - function
+ *
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+	void print_alphabet(void);
+	{
+		int i;
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
