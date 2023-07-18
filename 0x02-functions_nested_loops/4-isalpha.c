1c8cfe793ab58423c2bba9842b98f4e99cf68ac2
mahi7000 <mahlet7000@gmail.com>
2023-07-18 04:58:53 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - function
+ * @c - character to be tested
+ * Return: 1 if letter lowercase or uppercase, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
