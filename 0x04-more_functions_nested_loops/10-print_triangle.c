d09a82adec3b8390c8f4cb4fae4de36a532ebe92
mahi7000 <mahlet7000@gmail.com>
2023-07-20 06:36:14 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - function
+ * @size: number
+ */
+
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int i,j;
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
+
