f78377a53e576cc01d6ca553a793ad2c97f2993f
mahi7000 <mahlet7000@gmail.com>
2023-07-24 06:02:47 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - function
+ * @str: char
+ */
+
+void puts_half(char *str)
+{
+	int l, x;
+	int y;
+	l = 0;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		l++;
+	}
+	y = (l / 2);
+
+	if ((l % 2) == 1)
+	{
+		y = ((l + 1) / 2);
+	}
+
+	for (x = y; str[x] != '\0'; x++)
+		_putchar(str[x]);
+	_putchar('\n');
+}
