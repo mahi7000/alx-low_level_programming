ad22da788ba7de5092c9c507622603801d0d5e18
mahi7000 <mahlet7000@gmail.com>
2023-07-27 01:39:25 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - function
+ * @: char
+ * Return: char
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] > 96 && str[i] < 123)
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
