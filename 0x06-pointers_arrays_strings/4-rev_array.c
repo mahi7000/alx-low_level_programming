4977279e0da7543148c352c209b62fa6109bc85f
mahi7000 <mahlet7000@gmail.com>
2023-07-27 01:25:27 -0700

    4-rev_array ;

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - function
+ * @a: int
+ * @n: int
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int s;
+
+	i = 0;
+	while (i < n--)
+	{
+		s = a[i];
+		a[i] = a[n];
+		a[n] = s;
+		i++;
+	}
+}	
