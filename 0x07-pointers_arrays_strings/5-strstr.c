03eada1b1886d7ae3ed0481a1be1dd027d7591d0
mahi7000 <mahlet7000@gmail.com>
2023-07-31 05:17:36 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - function
+ * @haystack: pointer to char
+ * @needle: pointer to char
+ * Return: pointer or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *i = haystack;
+		char *j = needle;
+
+		while (*i == *j && *j != '\0')
+		{
+			i++;
+			j++;
+		}
+
+		if (*j == '\0')
+			return (haystack);
+	}
+	return (0);
+}
