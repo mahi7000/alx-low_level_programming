2cec1df56be020eee1bbc3b3ca6874e64659227c
mahi7000 <mahlet7000@gmail.com>
2023-08-02 00:51:02 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function
+ * @s: char
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+
+	return (i);
+}
