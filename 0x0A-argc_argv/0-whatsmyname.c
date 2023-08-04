c070819ec0e83f71b7a04fddbf7021c757b12548
mahi7000 <mahlet7000@gmail.com>
2023-08-04 05:57:13 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function
+ * @argc: int
+ * @argv: array of pointer to string
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
