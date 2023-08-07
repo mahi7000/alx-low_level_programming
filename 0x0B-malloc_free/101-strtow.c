a261e78897f704f0ffd01caf53a5035434e597b5
mahi7000 <mahlet7000@gmail.com>
2023-08-07 06:25:32 -0700

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - function
+ * @str: char
+ * Return: char
+ */
+
+char **strtow(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	if (str == NULL)
+		return (NULL);
+
+	return (NULL);
+}
