ff3aa8b971eeda43b7d01e5d4c2116ad2119476a
mahi7000 <mahlet7000@gmail.com>
2023-08-16 05:31:10 -0700

    3-calc.h

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,9 +1,9 @@
-#include "3-calc.h"
 #include <stdlib.h>
+#include "3-calc.h"
 
 /**
  * get_op_func - pointer to function
- * @s: strint
+ * @s: string
  * Return: int
  */
 
@@ -15,11 +15,11 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL},
+		{NULL, NULL}
 	};
+	int i;
 
-	int i = 0;
-
+	i = 0;
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
 
