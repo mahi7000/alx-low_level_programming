7a3a720a98069cdc5aed6e13533d3a125f57a193
mahi7000 <mahlet7000@gmail.com>
2023-08-17 04:33:16 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,24 +2,52 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * print_all - function
+ * @format: const string
+ * @...: arguments
+ * Return: void
+ */
+
 void print_all(const char * const format, ...)
 {
+	int i = 0;
+	char *str;
+	char *separator = "";
+
 	va_list any;
 
 	va_start(any, format);
 
-	struct anything {
-		char c;
-		int i;
-		float f;
-		char *s;
-	};
-	int i;
-
-	i = 0;
-
-
-
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%d", separator, va_arg(any, int));
+					break;
+				case 'i':
+					printf("%s%d", separator, va_arg(any, int));
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(any, double));
+					break;
+				case 's':
+					str = va_arg(any, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", separator, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			separator = ",";
+			i++;
+		}
+	}
 	printf("\n");
 
 	va_end(any);
