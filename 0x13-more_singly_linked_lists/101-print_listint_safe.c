da3eac68ab72636aff422b9fa74ac121e575e8e7
mahi7000 <mahlet7000@gmail.com>
2023-08-29 12:35:26 -0700

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - print
+ * @head: pointer
+ *
+ * Return: size_t
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count;
+
+	count = head->n;
+
+	return (count);
+}
