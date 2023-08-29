4c98fbece22fb211c84519a6ab8b56331e1186dc
mahi7000 <mahlet7000@gmail.com>
2023-08-29 12:42:06 -0700

    103-find_loop.c

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - function
+ * @head: head
+ *
+ * Return: listint_t
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *temp = head;
+
+	return (temp);
+}
