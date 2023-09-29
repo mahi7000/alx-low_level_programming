7bc664c3b8819f6a9ba7423a62f83ade75ae5f8b
mahi7000 <mahlet7000@gmail.com>
2023-09-29 11:48:48 -0700

    binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - convert number into binary
+ * @n: number
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, j;
+	unsigned long int m;
+
+	j = 0;
+	i = 27;
+	while (i >= 0)
+	{
+		m = n >> i;
+
+		if (1 & m)
+		{
+			_putchar('1');
+			j++;
+		}
+		else if (j)
+			_putchar('0');
+		i--;
+	}
+	if (!j)
+		_putchar('0');
+}
