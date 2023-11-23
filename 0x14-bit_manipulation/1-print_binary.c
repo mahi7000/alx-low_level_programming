061c82d8a676c7fe153156ac3b5c96c2128e54fe
mahi7000 <mahlet7000@gmail.com>
2023-11-23 03:38:22 -0800

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,5 +8,25 @@
 
 void print_binary(unsigned long int n)
 {
+	unsigned long int mask = 1UL << 31;
+	int leading_zeros = 1;
 
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (mask)
+	{
+		if (n & mask)
+		{
+			leading_zeros = 0;
+			_putchar('1');
+		}
+		else if (!leading_zeros)
+			_putchar('0');
+
+		mask >>= 1;
+	}
 }
