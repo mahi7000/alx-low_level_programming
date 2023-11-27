17a057e8594e0acd1784a0aafb036442890ddb65
mahi7000 <mahlet7000@gmail.com>
2023-11-27 06:02:12 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr;
+	int fd, wr, num_letters;
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
@@ -20,7 +20,11 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	wr = write(fd, text_content, sizeof(text_content) - 1);
+	num_letters = 0;
+	while (text_content[num_letters])
+		num_letters++;
+	
+	wr = write(fd, text_content, num_letters);
 
 	if (wr == -1)
 		return (-1);
