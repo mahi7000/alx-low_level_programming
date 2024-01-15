c107649b34091905d2923ccce6d4d90d00523ba6
mahi7000 <mahlet7000@gmail.com>
2024-01-15 04:01:46 -0800

    dynamic

---
+++ 0x18-dynamic_libraries/_isupper.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+int _isupper(int c)
+{
+	return (0);
+}
+
+int _isdigit(int c)
+{
+	return (0);
+}
+
+int _strlen(char *s)
+{
+	return (0);
+}
+
+void _puts(char *s)
+{
+}
+
+char *_strcpy(char *dest, char *src)
+{
+	return (dest);
+}
+
+char *_strcat(char *dest, char *src)
+{
+	return (src);
+}
---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+int _putchar(char c)
+{
+	return (0);
+}
+
+int _islower(int c)
+{
+	return (0);
+}
+
+int _isalpha(int c)
+{
+	return (0);
+}
+
+int _abs(int n)
+{
+	return (0);
+}
